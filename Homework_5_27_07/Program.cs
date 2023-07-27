9b4af96bb4bcd15c38d671ff4b201cd811f766ea
FirdavsAX <firdavsjumayev320@gmail.com>
2023-07-27 15:19:35 +0500

    it was not full

--- Homework_5_27_07/Program.cs
+++ Homework_5_27_07/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, enter string : ");
+
+            string enterStr = Console.ReadLine();
+            string repeatedWord = "";
+            string helpWord = "";
+
+            int repeatCount = 1;
+            int maxRepeat = 1;
+
+            for (int i = 0; i<enterStr.Length; i++)
+            {
+                for(int j = 1; j<enterStr.Length-i+1; j++)
+                {
+                    for (int k = 0; k<enterStr.Length-j; k++)
+                    {
+                        if (enterStr.Substring(i, j) == enterStr.Substring(k, j))
+                        {
+                            repeatCount++;
+                        }
+
+                    }
+
+                    if( repeatedWord.Length < enterStr.Substring(i,j).Length)
+                    {
+                        maxRepeat = repeatCount;
+                        repeatedWord = enterStr.Substring(i, j);
+                    }
+
+                    repeatCount=0;
+                }
+            }
+
+            Console.WriteLine(maxRepeat);
+            Console.WriteLine(repeatedWord);
         }
     }
 }
